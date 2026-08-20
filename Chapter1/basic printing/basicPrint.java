65c32a90e90f1a77c40f06a854561a68d6ddb858
Ansh Tewatia <astewatia1@stu.naperville203.org>
2026-08-20 14:02:00 -0500

    Basic print
    This is a lesson in basic printing

---
+++ Chapter1/basic printing/basicPrint.java
@@ -0,0 +1,47 @@
+
+/**
+ * Write a description of class basicPrint here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class basicPrint
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class basicPrint
+     */
+    public basicPrint()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.print("Hello world!");//This is to print a hello message
+        System.out.println("Hello again!");
+        System.out.print("this is on a new line");
+        /*
+         *this
+         *comment
+         *is on
+         *multiple
+         *lines
+         */
+    }
+}
