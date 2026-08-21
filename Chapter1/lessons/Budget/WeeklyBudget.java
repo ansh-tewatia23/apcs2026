ae3356f12cbf7db8c11b8f08c6efeacef2d7e7db
Ansh Tewatia <astewatia1@stu.naperville203.org>
2026-08-21 14:12:18 -0500

    Budget Lesson
    Start of lesson on basic data types and user inputs

---
+++ Chapter1/lessons/Budget/WeeklyBudget.java
@@ -0,0 +1,36 @@
+
+/**
+ *Weekly Budget
+ *Ansh Tewatia
+ *Last updated: 8/21/2026
+ */
+
+import java.util.Scanner;
+
+public class WeeklyBudget
+{
+    //Calculate weekly lunch spending and remaining money
+    public static void main(String[] args) {
+        String name;
+        double allowance, lunchPrice, totalCost, remaining;
+        int lunchesPerWeek;
+        
+        // Instantiate the Scanner object to read from the keyboard
+        Scanner scan = new Scanner (System.in);
+        
+        // Read a String (Object)
+        System.out.print("Enter your name: ");
+        name= scan.nextLine();
+        
+        // Read doubles (floating point number)
+        System.out.print("Enter your weekly allowance: $");
+        allowance = scan.nextDouble();
+        
+        System.out.print("Enter the price of one school lunch: $");
+        lunchPrice= scan.nextDouble();
+        
+        //Read an integer
+        System.out.print("Enter the number of school lunches you ordered this week");
+        lunchesPerWeek=scan.nextInt();
+    }   
+}
