8e96634f7a98eb80ac90a7691d6c4c7c70bb7598
Ansh Tewatia <astewatia1@stu.naperville203.org>
2026-08-26 14:23:08 -0500

    hello

---
+++ Chapter 2/PrizeGenerator.java
@@ -0,0 +1,46 @@
+
+/**
+ *Generate a random cash prize
+ *
+ * Ansh Tewatia
+ * Date Modified: 8/26/2026
+ */
+
+import java.util.Scanner;
+import java.text.NumberFormat;
+
+public class PrizeGenerator
+{
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        NumberFormat money= NumberFormat.getCurrencyInstance();
+        
+        System.out.println("Welcome to the APCSA prize simulator!");
+        System.out.println("--------------------------------------");
+        System.out.print("Please enter your name: ");
+        String name= scan.nextLine();
+        
+        System.out.print("How much money do you have? $");
+        double startBalance = scan.nextDouble();
+        
+        // Generate a random cash prize from $10-$100
+        //Math.random generates a number between 0(inclusive) and 1 (exclusive)
+        // (Math.random()*91 generates a number between 0 and 90.999
+        //(int)(Math.random() * 91 generates a number 0 and 90
+        // 10 + (int)(Math.random()*91) generates a number between 10 and 100
+        
+        int PrizeAmount= 10 + (int)(Math.random() * 91);
+        
+        
+        //Generate a random fee between 1 and 5
+        int FeeAmount= 1+ (int)(Math.random() * 5);
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
