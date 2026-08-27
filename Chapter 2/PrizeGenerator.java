bb57985f82e85186fd03ff9c9712a5699f0f9f75
Ansh Tewatia <astewatia1@stu.naperville203.org>
2026-08-27 14:11:40 -0500

    PrizeGen

--- Chapter 2/PrizeGenerator.java
+++ Chapter 2/PrizeGenerator.java
@@ -29,14 +29,26 @@ public class PrizeGenerator
         //(int)(Math.random() * 91 generates a number 0 and 90
         // 10 + (int)(Math.random()*91) generates a number between 10 and 100
         
-        int PrizeAmount= 10 + (int)(Math.random() * 91);
+        int prizeAmount= 10 + (int)(Math.random() * 91);
         
         
         //Generate a random fee between 1 and 5
-        int FeeAmount= 1+ (int)(Math.random() * 5);
+        int fee= 1+ (int)(Math.random() * 5);
         
         
+        double finalBalance=startBalance+prizeAmount-fee;
         
+        System.out.println("\n Congratulations "+ name + "! Here is your statement:");
+        System.out.println("========================================================");
+        
+        System.out.printf("%-25s %S%n", "Starting Balance", money.format(startBalance));
+        System.out.printf("%-25s %S%n", "Prize money", money.format(prizeAmount));
+        System.out.printf("%-25s %S%n", "Processing fee", money.format(fee));
+        System.out.println("----------------------------------");
+        System.out.printf("%-25s %S%n", "Final Balance", money.format(finalBalance));
+        System.out.println("====================================");
+
+    
         
         
         
